22a83603e26155eb662a2208b4e6d19abe79e373
LucasCosta100 <lucasdias46@outlook.com.br>
2025-10-15 21:37:06 -0300

    Finalizei a classe Funcionario com a devida herança

--- src/sistematarefas/model/Funcionario.java
+++ src/sistematarefas/model/Funcionario.java
@@ -1,10 +1,18 @@
 package sistematarefas.model;
 
-public class Funcionario {
+import java.time.LocalDate;
+
+public class Funcionario extends Usuario {
     private int idFuncionario;
     private double salario;
     private int idUsuario;
 
+    public Funcionario(int idFuncionario, double salario, int idUsuario, String nome, String email, String telefone, LocalDate dataCadastro, Endereco endereco, boolean ativo, Departamento departamento)
+    {
+        this.idFuncionario = idFuncionario;
+        this.salario = salario;
+        super(idUsuario, nome, email, telefone, dataCadastro, endereco, ativo, departamento);
+    }
     public int getIdFuncionario() { return idFuncionario; }
     public void setIdFuncionario(int idFuncionario) { this.idFuncionario = idFuncionario; }
     public double getSalario() { return salario; }
